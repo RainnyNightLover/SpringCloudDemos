b494da82932fc82466c15b6e1ea7828b087b3ba9
xihuanyuyetsinghua <xihuanyuye@126.com>
2018-07-20 16:10:20 +0800

    SpringcloudFeign

--- SpringCloudFeign/FeignConsumer/src/main/java/com/xihuanyuye/ConsumerController.java
+++ SpringCloudFeign/FeignConsumer/src/main/java/com/xihuanyuye/ConsumerController.java
@@ -3,16 +3,15 @@ package com.xihuanyuye;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class ConsumerController {
 
     @Autowired
-    private RestTemplate restTemplate;
+    private HomeClient homeClient;
 
     @GetMapping(value = "/hello")
     public String hello() {
-        return restTemplate.getForEntity("http://eureka-provider/", String.class).getBody();
+        return  homeClient.consumer();
     }
 }
---
+++ SpringCloudFeign/FeignConsumer/src/main/java/com/xihuanyuye/HomeClient.java
@@ -0,0 +1,11 @@
+package com.xihuanyuye;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@FeignClient("eureka-provider")
+public interface  HomeClient {
+
+    @GetMapping("/")
+    String consumer();
+}
